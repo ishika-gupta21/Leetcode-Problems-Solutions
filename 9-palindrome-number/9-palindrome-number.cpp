85e995b0b9c2491138e16d058e3494f0e2cdd33d
ishika-gupta21 <88900614+ishika-gupta21@users.noreply.github.com>
2022-05-06 00:40:00 +0530

    Time: 29 ms (22.02%), Space: 6 MB (34.06%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0||(x%10==0 && x!=0))
+            return false;
+        int r=0;
+        while(x>r)
+        {
+            int d=x%10;
+            r=r*10+d;
+            x=x/10;
+        }
+        return x==r||x==r/10;
+    }
+};

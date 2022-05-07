1556dc12e3fd37cdba9bafda1653fb1d09046331
ishika-gupta21 <88900614+ishika-gupta21@users.noreply.github.com>
2022-05-08 00:04:06 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (72.61%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -13,14 +13,11 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow=head;
         ListNode* fast=head;
-         if (head!=NULL)
-        {
             while (fast != NULL && fast->next != NULL)
             {
                 fast = fast->next->next;
                 slow = slow->next;
             }
-         }
         return slow;
     }
 };

84b348f944378780d09db3eac4b72572acbe66c6
ishika-gupta21 <88900614+ishika-gupta21@users.noreply.github.com>
2022-05-11 00:19:19 +0530

    Time: 36 ms (38.41%), Space: 19.3 MB (14.48%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int left=0,right=0;
+        int n=nums.size();
+        while(right<n){
+            if(nums[right]==0)
+                right++;
+            else
+            {
+                swap(nums[right],nums[left]);
+                right++;
+                left++;
+            }
+                
+        }
+    }
+};

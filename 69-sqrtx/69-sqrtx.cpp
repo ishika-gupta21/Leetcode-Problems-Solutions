61e4e251bf0f4a27c80f8e0d6b3fff19af0586f9
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-06-05 22:55:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (25.99%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -2,7 +2,23 @@ class Solution {
 public:
     int mySqrt(int x) 
     {
-        int square = sqrt(x);
-        return round(square);
+        /*int square = sqrt(x);
+        return round(square);*/
+        if(x<=1) return x;
+        int l = 0, h = x, ans;
+        while(l <= h)
+        {
+            int mid = (h+l)/2;
+            if(mid <= x/mid)
+            {
+                ans = mid;
+                l = mid + 1;
+            } 
+           else
+           {
+               h = mid - 1;
+           } 
+        }
+        return ans;
     }
 };

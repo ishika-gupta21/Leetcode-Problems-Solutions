ef741dc7a05ff2af4ea974890ddf6c0dd2d2e2d2
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-06-05 12:22:13 +0530

    Time: 6 ms (62.89%), Space: 9.4 MB (77.68%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order)
     {
-        map<char,int> hash;
+        unordered_map<char,int> map;
         
         for(int i=0; i<order.length(); i++){
-            hash[order[i]] = i+1;
+            map[order[i]] = i+1;
         }
         
         for(int i=1; i<words.size(); i++){
@@ -13,9 +13,9 @@ public:
             int n = words[i-1].length();
             int j=0;
             for(; j<m && j<n; j++){
-                if(hash[words[i][j]] > hash[words[i-1][j]])
+                if(map[words[i][j]] > map[words[i-1][j]])
                     break;
-                if(hash[words[i][j]] < hash[words[i-1][j]])
+                if(map[words[i][j]] < map[words[i-1][j]])
                     return false;
             }
             if(j==m && j<n){

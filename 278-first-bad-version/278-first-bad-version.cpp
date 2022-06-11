300feb9e1489f5cece94925eb4b6fdfa10a38313
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-06-11 23:41:40 +0530

    Time: 2 ms (41.85%), Space: 5.9 MB (23.14%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -9,11 +9,9 @@ public:
         while(i<=j)
         {
             mid=i+(j-i)/2;
-            
             if(isBadVersion(mid))
                 j=mid-1;
             else i=mid+1;
-                
         }
         return i;
     }

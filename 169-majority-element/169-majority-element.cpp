857eef4504e4388bfe3333b3802daeb7896591f9
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-06-22 23:02:30 +0530

    Time: 25 ms (66.61%), Space: 19.5 MB (58.81%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map <int,int> m;
+        for(int i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        int ans=0;
+        for(auto it:m)
+        {
+            if(it.second>n/2)
+                ans=it.first;
+        }
+        return ans;
+    }
+};

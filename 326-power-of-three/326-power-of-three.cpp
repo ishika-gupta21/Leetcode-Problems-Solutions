ee485a89b002316ac9b3ddfebb79ca44ac6cfea1
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-12 22:47:24 +0530

    Time: 35 ms (46.06%), Space: 5.9 MB (71.80%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        while (n>1) 
-        {
-            if (n%3) 
-                return false;
+        if(n==0)
+            return false;
+        while(n%3==0)
             n/=3;
-        }
-        return n==1;
+        return 
+            n==1;
         
     }
 };

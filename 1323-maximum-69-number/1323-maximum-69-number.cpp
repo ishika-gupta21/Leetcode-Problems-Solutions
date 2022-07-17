c2433e52455942afe27878195d094412157fa038
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-17 23:16:55 +0530

    Time: 5 ms (15.09%), Space: 6.1 MB (19.63%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximum69Number (int num) 
+    {
+        vector<int> v;
+        while(num!=0)
+        {
+            v.push_back(num%10);
+            num/=10;
+        }
+        bool te = false;
+        int n=0;
+        for(int i=v.size()-1;i>=0;i--)
+        {
+            if(te==false && v.at(i)==6)
+            {
+                n=(n*10)+9;
+                te=true;
+            }
+            else
+                n=(n*10)+v.at(i);
+        }
+        return n;
+    }
+};

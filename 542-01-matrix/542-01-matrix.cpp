2fde959976c321bc86e0e1ce6738dc7b72cc38bc
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-25 21:53:01 +0530

    Time: 111 ms (60.00%), Space: 30.1 MB (63.29%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,38 +1,42 @@
 class Solution {
+private:
+    bool checkValid(int i, int j, int m, int n)
+    {
+        if(i<0 || i>=m || j<0 || j>=n)   
+            return false;
+        return true;
+    }
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
     {
+        int rows = mat.size();
+        int cols = mat[0].size();
         queue<pair<int,int>> q;
-        for(int i=0;i<mat.size();i++)
+        
+        for(int i = 0;i<rows;i++)
         {
-            for(int j=0;j<mat[0].size();j++)
+            for(int j=0;j<cols;j++)
             {
-                if(mat[i][j]==0)
-                 q.push({i,j});
-                else
-                    mat[i][j]=-1;
+                if(mat[i][j]==0)    
+                    q.push({i,j});
+                else    
+                    mat[i][j] = -1;
             }
         }
-        vector<vector<int>> dir={{0,1},{-1,0},{1,0},{0,-1}};
-        int len=0;
+        int offset[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
         while(!q.empty())
         {
-            int size=q.size();
-            len++;
-            while(size--)
+            auto [i,j] = q.front();
+            for(auto [ni,nj]:offset)
             {
-                auto [i,j]=q.front();
-                q.pop();
-                for(vector<int> v: dir)
+                ni += i;  nj += j;
+                if(checkValid(ni,nj,rows,cols) && mat[ni][nj]==-1)
                 {
-                    int r=i+v[0];
-                    int co=j+v[1];
-                    if(r<0 || co<0 || r==mat.size() || co==mat[0].size() || mat[r][co]!=-1)
-                        continue;
-                    mat[r][co]=len;
-                    q.push({r,co});
+                    q.push({ni,nj});
+                    mat[ni][nj] = mat[i][j] + 1;
                 }
             }
+            q.pop();
         }
         return mat;
     }

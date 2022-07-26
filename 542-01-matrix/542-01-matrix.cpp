40f5be29ff043d6e4c780ca8a86b36a52db2d0e2
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-26 15:05:39 +0530

    Time: 86 ms (87.61%), Space: 29.9 MB (73.91%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -5,23 +5,30 @@ private:
         return true;
     }
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
+    {
         int rows = mat.size();
         int cols = mat[0].size();
         queue<pair<int,int>> q;
-        
-        for(int i = 0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(mat[i][j]==0)    q.push({i,j});
-                else    mat[i][j] = -1;
+        for(int i = 0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(mat[i][j]==0)    
+                    q.push({i,j});
+                else    
+                    mat[i][j] = -1;
             }
         }
         int offset[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        while(!q.empty()){
+        while(!q.empty())
+        {
             auto [i,j] = q.front();
-            for(auto [ni,nj]:offset){
+            for(auto [ni,nj]:offset)
+            {
                 ni += i;  nj += j;
-                if(checkValid(ni,nj,rows,cols) && mat[ni][nj]==-1){
+                if(checkValid(ni,nj,rows,cols) && mat[ni][nj]==-1)
+                {
                     q.push({ni,nj});
                     mat[ni][nj] = mat[i][j] + 1;
                 }

57aea8749e0d879add7865ecdcbe4bc8e368372a
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-29 15:35:45 +0530

    Time: 31 ms (48.03%), Space: 19.6 MB (12.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& nums)
+    {
         int n=nums.size();
         unordered_map <int,int> m;
         for(int i=0;i<n;i++)

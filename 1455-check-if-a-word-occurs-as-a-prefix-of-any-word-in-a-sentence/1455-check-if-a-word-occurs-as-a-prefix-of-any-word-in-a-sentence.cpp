14cfa1b0cfbd9789532f5f79e4f9b36d0c4535a8
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-07-30 20:54:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.38%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -2,23 +2,33 @@ class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord)
     {
-        string s="";
-        int c=0;
-        for(int i=0;i<sentence.length();i++)
+        // string s="";
+        // int c=0;
+        // for(int i=0;i<sentence.length();i++)
+        // {
+        //     if(sentence[i]!=' ')
+        //         s+=sentence[i];
+        //     else
+        //     {
+        //         c++;
+        //         if(s.find(searchWord)==0)
+        //             return c;
+        //         s="";
+        //     }
+        // }
+        // c++;
+        // if(s.find(searchWord)==0)
+        //     return c;
+        // return -1;
+        stringstream ss(sentence);
+        string word="";
+        int count=1;
+        while(ss>>word)
         {
-            if(sentence[i]!=' ')
-                s+=sentence[i];
-            else
-            {
-                c++;
-                if(s.find(searchWord)==0)
-                    return c;
-                s="";
-            }
+            if(word.find(searchWord)==0)
+                return count;
+            count++;
         }
-        c++;
-        if(s.find(searchWord)==0)
-            return c;
         return -1;
     }
 };

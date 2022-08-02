45c72b7dbd14b65d688e77ea8a09e7cac4427b60
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-08-03 00:06:09 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int uniquePaths(int m, int n)
     {
-        vector<vector<int>> paths(n, vector<int>(m));
-        for(int c = 0; c < m ; ++c)
-            paths[n-1][c] = 1;
-        for(int r = 0; r < n ; ++r)
-            paths[r][m-1] = 1;
-        for(int r = n-2; r >= 0 ; --r)
-            for(int c = m-2; c >= 0 ; --c)
-                paths[r][c] = paths[r][c+1] + paths[r+1][c];
-        return paths[0][0];
+        vector<vector<int>> dp(m , vector<int>(n, -1));
+        dp[m-1][n-1] = 1; 
+        for(int i = m-2 ; i >= 0 ; i--)
+        {
+            for(int j = n-2 ; j >= 0 ; j--)
+            {
+                    int x = dp[i+1][j];
+                    int y = dp[i][j+1];
+                    dp[i][j] = x+y;
+            }
+        }
+        return abs(dp[0][0]);
     }
 };

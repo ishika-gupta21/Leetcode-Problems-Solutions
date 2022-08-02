bdae426996c578c0569a812cefd585176444009a
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-08-02 23:21:45 +0530

    Time: 93 ms (78.17%), Space: 34.3 MB (93.02%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int leastInterval(vector<char>& v, int n)
     {
-        int sz=v.size();
-      if(!n)
-        return sz;
-      vector<int>mp(26,0);
-      for(char i:v)
-        ++mp[i-'A'];
-      sort(mp.begin(),mp.end());
-      while(!mp[0])
-        mp.erase(mp.begin());
-      int gapcnt=mp.back()-1,gaps=gapcnt*n;
-      mp.pop_back();
-      while(!mp.empty())
-      {
-        int i=mp.back();
-        mp.pop_back();
-        gaps-=min(i,gapcnt);
-      }
-      return gaps<0?sz:gaps+sz;
+          int sz=v.size();
+          if(!n)
+            return sz;
+          vector<int>mp(26,0);
+          for(char i:v)
+            ++mp[i-'A'];
+          sort(mp.begin(),mp.end());
+          while(!mp[0])
+            mp.erase(mp.begin());
+          int gapcnt=mp.back()-1,gaps=gapcnt*n;
+          mp.pop_back();
+          while(!mp.empty())
+          {
+            int i=mp.back();
+            mp.pop_back();
+            gaps-=min(i,gapcnt);
+          }
+          return gaps<0?sz:gaps+sz;
     }
 };

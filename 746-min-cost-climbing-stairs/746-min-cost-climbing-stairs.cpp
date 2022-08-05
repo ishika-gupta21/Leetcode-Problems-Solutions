e777acd0cf3dba9eabf9351ab343a5819bb5535e
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-08-05 16:19:05 +0530

    Time: 11 ms (54.09%), Space: 13.6 MB (96.09%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -2,14 +2,9 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) 
     {
-        int n = cost.size();
-        int a[n];
-        a[0] = 0;
-        a[1] = 0;
-        for(int i = 2 ; i < n ; i++)
-        {
-              a[i] = min(a[i-1]+cost[i-1],a[i-2]+cost[i-2]);
-        }
-        return min(a[n-1]+cost[n-1],a[n-2]+cost[n-2]);
+        int lim = cost.size(), i = 0, j = 1, k = 2;
+        while(k != lim) 
+            cost[k++] += min(cost[i++], cost[j++]);
+        return min(cost[i],cost[j]);
     }
 };

e39f85c9db410735b8563a341cb3a6f16fd6a5df
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-08-09 14:57:01 +0530

    Time: 33 ms (38.90%), Space: 14.6 MB (69.38%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& a) 
+    {
+        int n = a.size();
+        if(n==1) 
+            return {-1};
+        int g = a[n-1];
+        vector<int> v(n,-1);
+        for(int i=n-2;i>=0;i--)
+        {
+            v[i] = g;
+            g = max(g,a[i]);
+        }
+        return v;
+    }
+};

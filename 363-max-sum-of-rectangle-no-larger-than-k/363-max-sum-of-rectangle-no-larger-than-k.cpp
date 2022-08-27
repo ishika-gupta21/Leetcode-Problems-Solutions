113bb0a08a82147472edb11d5eb0ad0261007b90
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-08-27 11:59:25 +0530

    Time: 152 ms (99.91%), Space: 10.5 MB (97.15%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& m, int k)
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k)
     {
-        int res = INT_MIN, rows = m.size(), cols = m[0].size();
-        for (int l = 0; l < cols; ++l)
+        int ans = INT_MIN;
+        int R = matrix.size();
+        int C = matrix[0].size();
+        int arr[R];
+        for(int l = 0; l < C; ++l) 
         {
-            vector<int> sums(rows);
-            for (int r = l; r < cols; ++r)
+            memset(arr, 0, sizeof(arr));
+            for(int r = l; r < C; ++r) 
             {
-                for (int i = 0; i < rows; ++i) 
-                    sums[i] += m[i][r];
-                set<int> s = {0};
-                int run_sum = 0;
-                for (int sum : sums)
+                for(int up = 0; up < R; ++up)
+                    arr[up] += matrix[up][r]; 
+                int sum = 0, g = INT_MIN;
+                for(int up = 0; up < R; ++up) 
                 {
-                    run_sum += sum;
-                    auto it = s.lower_bound(run_sum - k);
-                    if (it != end(s))
-                        res = max(res, run_sum - *it);
-                    s.insert(run_sum);
+                    sum = max(sum + arr[up], arr[up]);
+                    g = max(g, sum);
+                    if(g == k) 
+                        return k;
+                }
+                if(g < k) 
+                    ans = max(ans, g);
+                else 
+                {
+                    for(int up1 = 0; up1 < R; ++up1) 
+                    {
+                        sum = 0;
+                        for(int up2 = up1; up2 < R; ++up2) 
+                        {
+                            sum += arr[up2];
+                            if(sum <= k) 
+                                ans = max(ans, sum);
+                        }
+                        if(ans == k) 
+                            return k;
+                    }
                 }
             }
         }
-        return res;
+        return ans;
     }
 };

4ac75bb8c6a8abf8a62223c59e0d5e837b0badb2
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-09-06 19:26:53 +0530

    Time: 553 ms (38.04%), Space: 106.7 MB (37.33%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -4,7 +4,7 @@ public:
     {
         vector<int> ans;
         vector<int> indeg(n,0);
-        for(auto i: edges)  
+        for(auto i:edges)  
             indeg[i[1]]++;
         for(int i=0;i<indeg.size();i++)  
             if(indeg[i]==0) 

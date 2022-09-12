e2fe78edf8565ae02a893f2b6c5d1e12df6321d7
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-09-12 21:10:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (76.36%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -2,40 +2,44 @@ class Solution {
 public:
     int divide(int dividend, int divisor)
     {
-        int c=0;
-        bool f=false;
-        if(dividend == INT_MIN && divisor == -1)
+        // int c=0;
+        // bool f=false;
+        // if(dividend == INT_MIN && divisor == -1)
+        //     return INT_MAX;
+        // else if(dividend == INT_MIN && divisor == 1)
+        //     return INT_MIN;
+        // else if(dividend==INT_MIN || divisor==INT_MIN)
+        //     return dividend/divisor;
+        // else if(divisor==1)
+        //     return dividend;
+        // else if(divisor==-1)
+        //     return -dividend;
+        // else if(divisor>0 && dividend<0)
+        // {
+        //     f=true;
+        //     dividend*=-1;
+        // }
+        // else if(divisor<0 && dividend>0)
+        // {
+        //     f=true;
+        //     divisor*=-1;
+        // }
+        // else if(divisor<0 && dividend<0)
+        // {
+        //     divisor=divisor*-1;
+        //     dividend=dividend*-1;
+        // }
+        // while(dividend>=divisor)
+        // {
+        //     dividend-=divisor;
+        //     c++;
+        // }
+        // if(f==true)
+        //     c*=-1;
+        // return c;
+        if((dividend == INT_MIN)&&(divisor == -1)) 
             return INT_MAX;
-        else if(dividend == INT_MIN && divisor == 1)
-            return INT_MIN;
-        else if(dividend==INT_MIN || divisor==INT_MIN)
-            return dividend/divisor;
-        else if(divisor==1)
-            return dividend;
-        else if(divisor==-1)
-            return -dividend;
-        else if(divisor>0 && dividend<0)
-        {
-            f=true;
-            dividend*=-1;
-        }
-        else if(divisor<0 && dividend>0)
-        {
-            f=true;
-            divisor*=-1;
-        }
-        else if(divisor<0 && dividend<0)
-        {
-            divisor=divisor*-1;
-            dividend=dividend*-1;
-        }
-        while(dividend>=divisor)
-        {
-            dividend-=divisor;
-            c++;
-        }
-        if(f==true)
-            c*=-1;
-        return c;
+        return dividend/divisor;
+
     }
 };

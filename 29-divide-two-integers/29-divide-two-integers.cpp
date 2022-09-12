22993872650651340daff17ec9b6640363674b8f
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-09-12 21:05:20 +0530

    Time: 1745 ms (5.03%), Space: 5.7 MB (95.99%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -6,25 +6,25 @@ public:
         bool f=false;
         if(dividend == INT_MIN && divisor == -1)
             return INT_MAX;
-        if(dividend == INT_MIN && divisor == 1)
+        else if(dividend == INT_MIN && divisor == 1)
             return INT_MIN;
-        if(dividend==INT_MIN || divisor==INT_MIN)
+        else if(dividend==INT_MIN || divisor==INT_MIN)
             return dividend/divisor;
-        if(divisor==1)
+        else if(divisor==1)
             return dividend;
-        if(divisor==-1)
+        else if(divisor==-1)
             return -dividend;
-        if(divisor>0 && dividend<0)
+        else if(divisor>0 && dividend<0)
         {
             f=true;
             dividend*=-1;
         }
-        if(divisor<0 && dividend>0)
+        else if(divisor<0 && dividend>0)
         {
             f=true;
             divisor*=-1;
         }
-        if(divisor<0 && dividend<0)
+        else if(divisor<0 && dividend<0)
         {
             divisor=divisor*-1;
             dividend=dividend*-1;

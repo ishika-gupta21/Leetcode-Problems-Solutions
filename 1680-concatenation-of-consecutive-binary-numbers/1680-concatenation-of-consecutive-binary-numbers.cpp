7cdc36a4d85d3109d3c237f8450138c20c1d3621
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-09-23 14:11:32 +0530

    Time: 35 ms (92.93%), Space: 5.8 MB (95.65%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) 
+    {
+        long ans=1,len=4;
+        for (int i=2;i<=n;i++) 
+        {
+            if (i==len) 
+                len*=2;
+            ans=(ans*len+i)%1000000007;
+        }
+        return ans;
+    }
+};

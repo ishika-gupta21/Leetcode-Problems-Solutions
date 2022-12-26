558c69d6a4d71d7aa0b202650b95aeaef38a8fbb
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2022-12-26 21:57:29 +0530

    Time: 61 ms (96.57%), Space: 48.2 MB (94.13%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& v)
+    {
+        int n=v.size();
+        int res=0;
+        int t=n-1;
+        if(t==0)
+            return true;
+        if(v[0]==0)
+            return false;
+        for(int i=0;i<n-1;i++)
+        {
+            if(v[i]+i>res and res>=i)
+                res=v[i]+i;
+        }
+        if(res>=t)
+            return true;
+        return false;
+    }
+};

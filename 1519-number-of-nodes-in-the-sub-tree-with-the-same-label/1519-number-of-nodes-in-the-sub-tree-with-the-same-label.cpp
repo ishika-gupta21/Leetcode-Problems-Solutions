540c32e1e65ea887ff06eee3f90647ac31958160
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2023-01-12 23:09:05 +0530

    Time: 626 ms (79.26%), Space: 227.3 MB (38.52%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+   void dfs(vector<vector<int>>& graph, vector<int>& ans, int par, int node, string& labels, vector<int> ancestor) {
+        
+        int same_label_ancestor = ancestor[labels[node] - 'a'];
+		ancestor[labels[node] - 'a'] = node;
+        for (int child : graph[node]) 
+        {
+            if (child != par)
+                dfs(graph, ans, node, child, labels, ancestor);
+        }
+        ans[node]++;
+        if (same_label_ancestor != -1)
+            ans[same_label_ancestor] += ans[node];  
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) 
+    {
+        vector<vector<int>> graph(n, vector<int>());
+        vector<int> ans(n, 0);
+        vector<int> ancestor(26, -1);
+        for (auto edge : edges) 
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        dfs(graph, ans, -1, 0, labels, ancestor);
+        return ans; 
+    }
+};

1309be0330854f2c3f650bff9296774c2c84916d
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2023-02-22 13:20:03 +0530

    Time: 4 ms (39.54%), Space: 6.2 MB (81.64%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        int f=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else
+            {
+                if(!st.empty() && ((s[i]==')' && st.top()=='(') || (s[i]=='}' && st.top()=='{') || (s[i]==']' && st.top()=='[')))
+                   st.pop();
+                else
+                   return false;
+            }
+        }
+        if(st.empty())
+            return true;
+        else
+            return false;
+        
+    }
+};

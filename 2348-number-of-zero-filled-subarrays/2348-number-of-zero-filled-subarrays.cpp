14331d5a0668b5ca400e65d5884da7c8f87b5e73
Ishika Gupta <88900614+ishika-gupta21@users.noreply.github.com>
2023-03-21 22:23:08 +0530

    Time: 168 ms (75.72%), Space: 107.5 MB (86.43%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) 
+    {
+        long long ans=0;
+        int n=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                n++;
+                ans+=n;
+            }
+            else
+                n=0;
+        }
+        return ans;
+    }
+};
